7ecbb718f0776e1229ad5e27f843d77d91114c8f
Matthew Padilla <matthewpadilla02@gmail.com>
2024-10-25 19:03:05 -0400

    Switched the code for the header files for driveControl.h and driveTrain.h

--- include/driveControl.h
+++ include/driveControl.h
@@ -1,28 +1,15 @@
-#ifndef _DRIVETRAIN
-#define _DRIVETRAIN
+#ifndef _DRIVE_CONTROL
+#define _DRIVE_CONTROL
 
 #define PROS_USE_SIMPLE_NAMES
 #define PROS_USE_LITERALS
 #include "api.h"
-#include "lemlib/api.hpp"
 #include "init/devices.h"
+#include "init/utility.h"
 #include "lemlib/chassis/chassis.hpp"
+#include "pros/misc.h"
+#include "drive/drivetrain.h"
 
-float customDriveCurve(float input, float scale);
-
-// Drivetrain
-extern lemlib::Drivetrain drivetrain;
-
-// Odometry
-extern lemlib::TrackingWheel horizontal;
-
-extern lemlib::OdomSensors sensors;
-
-// PID
-extern lemlib::ControllerSettings lateralController;
-extern lemlib::ControllerSettings angularController;
-
-// Putting it all together
-extern lemlib::Chassis chassis;
+void driveControl();
 
 #endif
