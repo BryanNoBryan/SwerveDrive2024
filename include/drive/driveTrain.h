b2f03ac412bbf62b2e1a4ae1f642882f4ea1fd37
Matthew Padilla <matthewpadilla02@gmail.com>
2024-10-25 10:02:42 -0400

    Update driveTrain.h with the proper header code

--- include/drive/driveTrain.h
+++ include/drive/driveTrain.h
@@ -1,15 +1,28 @@
-#ifndef _DRIVE_CONTROL
-#define _DRIVE_CONTROL
+#ifndef _DRIVETRAIN
+#define _DRIVETRAIN
 
 #define PROS_USE_SIMPLE_NAMES
 #define PROS_USE_LITERALS
 #include "api.h"
+#include "lemlib/api.hpp"
 #include "init/devices.h"
-#include "init/utility.h"
 #include "lemlib/chassis/chassis.hpp"
-#include "pros/misc.h"
-#include "drive/drivetrain.h"
 
-void driveControl();
+float customDriveCurve(float input, float scale);
+
+// Drivetrain
+extern lemlib::Drivetrain drivetrain;
+
+// Odometry
+extern lemlib::TrackingWheel horizontal;
+
+extern lemlib::OdomSensors sensors;
+
+// PID
+extern lemlib::ControllerSettings lateralController;
+extern lemlib::ControllerSettings angularController;
+
+// Putting it all together
+extern lemlib::Chassis chassis;
 
 #endif
