da4f279a2f0779a9b96c5c9ff70e3030b6dabd46
Bryan Liu <121914672+BryanNoBryan@users.noreply.github.com>
2025-01-30 00:46:30 -0500

    checkpoint pursuit

--- src/drive/purePursuit/CheckpointPursuit.cpp
+++ src/drive/purePursuit/CheckpointPursuit.cpp
@@ -7,10 +7,21 @@ class CheckpointPursuit {
     CheckpointPursuit(Path path) : path(path) {};
 
     void run() {
-        
+        //the vector that we need to traverse to finish the path
+        Pose diffPose = path.start_pos - path.end_pos;
+
+        // 1. move heading in direction of vector
+
+
+
+        // 2. move to end_pos
+
+
+
+        // 3. move heading to final_pos
     }
 
     bool finished() {
-        
+       path.reachedTarget();
     }
 }
